d8b7227f64efe02e06529c1b5445cc47ce02fb24
JaviLeL <javier212002@gmail.com>
2025-01-22 12:40:56 +0100

    Wed Jan 22 12:40:56 CET 2025

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -3,7 +3,7 @@ return {
     init = function()
         vim.g.vimwiki_list = {
             {
-            path = '~/Documents/.life/',
+            path = '~/Documents/life/',
             -- Style creator
             -- syntax = 'default',
             -- Style Markdown
